f3b69ab8c25a54e2af7a0a39d87ff3b1f627766b
alex <alexellis2@gmail.com>
2016-05-19 14:17:44 +0000

    Downloader without breaking stack

--- downloader/index.js
+++ downloader/index.js
@@ -2,6 +2,8 @@
 
 let request = require('request');
 let Parser = require('./parser.js');
+let async = require('async');
+let fs = require('fs');
 
 function getSiteMap() {
   return new Promise((resolve, reject) => {
@@ -16,10 +18,40 @@ function getSiteMap() {
 
 getSiteMap()
 .then((urls)=> {
-   urls.forEach((url) => {
-      console.log("Downloading: " + url);
-   });
+    return new Promise((resolve, reject) => {
+        var tasks = [];
+        urls.forEach((url) => {
+            if(url.length > 0) {
+                let outfile = url.substring(url.lastIndexOf("\/\/")+2);
+                outfile = outfile.replace(/\//g, "_");
+                outfile = outfile.replace(/\./g, "_") + ".html";
+                tasks.push({ url: url, outfile: outfile });
+            }
+        });
+        resolve(tasks);
+    });
+})
+.then(tasks => {
+  console.log("tasks ready " + tasks.length);
+  var q = async.queue((task, done) => {
+      process.stdout.write(".")
+      request.get(task.url, (err,res,body) => {
+          fs.writeFile("html/" + task.outfile, body, "utf8", () => {
+              process.stdout.write("-")
+            // console.log("Written: " + task.outfile);
+            setImmediate(done);              
+          });
+      });
+  }, 10);
+  q.drain = () => {
+      console.log("Everything has been downloaded.")
+  };
+  for(let i = 0;i < tasks.length; i++) {
+     q.push(tasks[i]);
+  }
+//  tasks.forEach((t)=> { q.push(t); });
 })
 .catch((e) => {
    console.error(e);
 });
+
