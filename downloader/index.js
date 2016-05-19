8b6eb3790b8a700c43870ce379e6ed8f84243aa1
Alex Ellis <alexellis2@gmail.com>
2016-05-19 21:30:00 +0100

    Nice-ing up some of the code.
    Making sure HTML folder exists for output files

--- downloader/index.js
+++ downloader/index.js
@@ -5,6 +5,8 @@ let Parser = require('./parser.js');
 let async = require('async');
 let fs = require('fs');
 
+let concurrent_http_requests = 10;
+
 function getSiteMap() {
   return new Promise((resolve, reject) => {
      request.get({"url": "http://www.bbc.co.uk/food/sitemap.xml"}, (err,res,body) => {
@@ -16,15 +18,20 @@ function getSiteMap() {
    });
 }
 
+function getOutfile(url) {
+   let outfile = url.substring(url.lastIndexOf("\/\/")+2);
+   outfile = outfile.replace(/\//g, "_");
+   outfile = outfile.replace(/\./g, "_") + ".html";
+   return outfile;
+}
+
 getSiteMap()
 .then((urls)=> {
     return new Promise((resolve, reject) => {
         var tasks = [];
         urls.forEach((url) => {
             if(url.length > 0) {
-                let outfile = url.substring(url.lastIndexOf("\/\/")+2);
-                outfile = outfile.replace(/\//g, "_");
-                outfile = outfile.replace(/\./g, "_") + ".html";
+                let outfile = getOutfile(url);
                 tasks.push({ url: url, outfile: outfile });
             }
         });
@@ -32,17 +39,19 @@ getSiteMap()
     });
 })
 .then(tasks => {
-  console.log("tasks ready " + tasks.length);
+  console.log("Downloading: " + tasks.length+ " URLs.");
   var q = async.queue((task, done) => {
       process.stdout.write(".")
-      request.get(task.url, (err,res,body) => {
+
+      var req = {url:task.url, headers: {"User-Agent":"auntiesreciples downloader" }};
+      request.get(req, (err,res,body) => {
           fs.writeFile("html/" + task.outfile, body, "utf8", () => {
               process.stdout.write("-")
             // console.log("Written: " + task.outfile);
             setImmediate(done);              
           });
       });
-  }, 10);
+  }, concurrent_http_requests);
   q.drain = () => {
       console.log("Everything has been downloaded.")
   };
